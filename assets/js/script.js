764208ba0970524ef29f79fd845cbb9b7db75bea
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-18 15:28:14 +0000

    add subtraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,9 @@ function calculateCorrectAnswer() {
     else if (operator === "x" ) {
         return [operand1 * operand2, "multiply"];
     }
+    else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -122,7 +125,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
