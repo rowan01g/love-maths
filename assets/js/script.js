9dfedcbf50d8777d8e854bb30e9fdae0a1c33bbf
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-18 15:22:02 +0000

    add multiplication

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,17 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-} else {
+}
+else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+}
+else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
+}
+else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
+}
+else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
 }
@@ -74,6 +84,9 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } 
+    else if (operator === "x" ) {
+        return [operand1 * operand2, "multiply"];
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -113,7 +126,15 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion() {
 
 }
 
